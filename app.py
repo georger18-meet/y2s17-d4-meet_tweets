a1dabf514e2dd6e8c0472ebd8af6ed939d04d60a
mnowik <nowicki.matthieu@gmail.com>
2017-06-14 17:20:48 -0700

    creation of a basic flask app

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
