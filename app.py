eacf4ea329e88dc761f471a57dee728d09d5d052
mnowik <nowicki.matthieu@gmail.com>
2017-07-25 16:45:11 +0300

    add correct app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+import sys
 
-@app.route('/<name>')
-def hello_world(name=None):
-    return render_template('hello.html', name=name)
+# SQLAlchemy stuff
+from model import Base, Tweet
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('sqlite:///tweetlab.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def my_feed():
+    tweets = [] # CHANGE THIS TO READ YOUR TWEETS FROM THE DATABASE
+    return render_template('my_feed.html', tweets=tweets)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_tweet():
+    return render_template('add_tweet.html')
