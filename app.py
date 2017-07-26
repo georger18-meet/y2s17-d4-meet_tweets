ae28093923a27e618ec33f710a323652fb023f5b
mnowik <nowicki.matthieu@gmail.com>
2017-07-27 00:19:29 +0300

    push app with lab 1 to 3

--- app.py
+++ app.py
@@ -14,10 +14,26 @@ session = DBSession()
 
 @app.route('/')
 def my_feed():
-    tweets = [] # CHANGE THIS TO READ YOUR TWEETS FROM THE DATABASE
+    tweets = session.query(Tweet).all()
     return render_template('my_feed.html', tweets=tweets)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_tweet():
     return render_template('add_tweet.html')
+
+
+@app.route('/BROKEN-ROUTE-FOR-YOU-TO-FIX')
+def edit_tweet(tweet_id):
+    tweet = # RETRIEVE TWEET THAT HAVE id=tweet_id FROM THE DATABASE
+    return render_template('edit_tweet.html', tweet=tweet)
+
+
+@app.route('/delete/<int:tweet_id')
+def delete_tweet(tweet_id):
+    if request.method == 'GET':
+      tweet = # RETRIEVE TWEET THAT HAVE id=tweet_id FROM THE DATABASE
+      return render_template('delete_tweet.html', tweet=tweet)
+    else:
+      # DELETE TWEET THAT HAVE id=tweet_id FROM THE DATABASE
+      return redirect(url_for('my_feed')) 
